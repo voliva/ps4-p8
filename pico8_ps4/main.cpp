4bf7b4683208c97de70e8a3b9dfcb976bdcd863d
Víctor Oliva <olivarra1@gmail.com>
2021-12-30 22:53:29 +0100

    audio wave generator

--- pico8_ps4/main.cpp
+++ pico8_ps4/main.cpp
@@ -10,6 +10,7 @@
 #include "events.h"
 #include "lua_state.h"
 #include "machine_state.h"
+#include <math.h>
 
 #ifdef __PS4__
 #define MINEWALKER "/app0/assets/misc/minewalker.p8.png"
@@ -24,9 +25,76 @@ Log DEBUGLOG = logger.log("main");
 MachineState* machineState;
 int main(void)
 {
+	DEBUGLOG << "Initialize" << ENDL;
 	if (!init_renderer()) {
 		return -1;
 	}
+
+	DEBUGLOG << "Initialized" << ENDL;
+
+	// the representation of our audio device in SDL:
+	SDL_AudioDeviceID audio_device;
+
+	// opening an audio device:
+	SDL_AudioSpec audio_spec;
+	SDL_zero(audio_spec);
+	audio_spec.freq = 44100;
+	audio_spec.format = AUDIO_S16SYS;
+	audio_spec.channels = 1;
+	audio_spec.samples = 1024;
+	audio_spec.callback = NULL;
+
+	DEBUGLOG << "Open audio device" << ENDL;
+	audio_device = SDL_OpenAudioDevice(
+		NULL, 0, &audio_spec, NULL, 0);
+	DEBUGLOG << "Opened? " << audio_device << ENDL;
+
+	// pushing 3 seconds of samples to the audio buffer:
+	float x = 0;
+	float hz = 440;
+	float inc = hz * 2 * M_PI / audio_spec.freq;
+	int AMP = 5000;
+	for (int i = 0; i < audio_spec.freq * 1; i++) {
+		x += inc;
+
+		// SDL_QueueAudio expects a signed 16-bit value
+		// note: "5000" here is just gain so that we will hear something
+		// SIN
+		int16_t sample = sin(x) * AMP * 1.5;
+
+		// Square
+		/*int16_t sample = 0;
+		if ((int)(x / M_PI) % 2) {
+			sample = AMP;
+		}else{
+			sample = -AMP;
+		}*/
+
+		// Triangle
+		/*float mod = fmod(x, 2 * M_PI);
+		int16_t sample = (abs(mod / M_PI - 1) - 0.5) * 2 * AMP * 2;*/
+
+		// Sawtooth
+		/*float mod = fmod(x, 2 * M_PI);
+		int16_t sample = (mod / M_PI - 1) * AMP * 2;*/
+
+		// White noise
+		// int16_t sample = AMP * rand() / RAND_MAX;
+
+		const int sample_size = sizeof(int16_t) * 1;
+		SDL_QueueAudio(audio_device, &sample, sample_size);
+	}
+
+	DEBUGLOG << "Queued, playing" << ENDL;
+	// unpausing the audio device (starts playing):
+	SDL_PauseAudioDevice(audio_device, 0);
+
+	DEBUGLOG << "Wait" << ENDL;
+	SDL_Delay(1000);
+	return -1;
+
+	DEBUGLOG << "Close" << ENDL;
+	SDL_CloseAudioDevice(audio_device);
 
 	DEBUGLOG << "Loading cartridge..." << ENDL;
 	Cartridge* r = load_from_png(MINEWALKER);
