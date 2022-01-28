5728462bae68a29dd627c72a919ffba10233319a
Víctor Oliva <olivarra1@gmail.com>
2022-01-28 23:40:40 +0100

    remove obsolete tests

--- pico8_ps4/test/cartridge_test.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-./mac.sh skiprun
-cd test
-
-set -e # Exit script when any error happens
-
-includedFiles="../mac_build/cartridge.o ../mac_build/http.o ../mac_build/log.o"
-clang++ cartridge_test.cpp $includedFiles ../../lua/mac_build/*.o -std=c++11 -lSDL2 -lcurl -I"../../lua" -o "cartridge_test.exe"
-"./cartridge_test.exe"
